908b35b9127ca352421561fcce204ab9af8a5cdd
gitx1 <mygitx1@gmail.com>
2012-07-18 11:01:46 -0500

    Initial Commit

---
+++ TestGitHub/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TestGitHub
+//
+//  Created by Temporal on 18/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
